e3291d8eb647365dfae9071bbe4b3b51d58152ea
Afroz <afrozalikhan81@gmail.com>
2023-12-18 12:04:51 +0530

    First shell script

--- 02-print.sh
+++ 02-print.sh
@@ -2,9 +2,9 @@
 
  echo  Hello world
 
- echo -e "\e[31Hello in Red Color\e[0m"
- echo -e "\e[32Hello in Green Color\e[0m"
- echo -e "\e[33Hello in Yellow color\e[0m"
- echo -e "\e[34Hello in Blue color\e[0m"
- echo -e "\e[35Hello in Magenta color\e[0m"
- echo -e "\e[36Hello in Cyan color\e[0m"
+ echo -e "\e[31mHello in Red Color\e[0m"
+ echo -e "\e[32mHello in Green Color\e[0m"
+ echo -e "\e[33mHello in Yellow color\e[0m"
+ echo -e "\e[34mHello in Blue color\e[0m"
+ echo -e "\e[35mHello in Magenta color\e[0m"
+ echo -e "\e[36mHello in Cyan color\e[0m"
