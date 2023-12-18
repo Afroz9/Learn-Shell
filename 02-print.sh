26c58bd01e0d5f93fdf3a9f05abc1626627376dd
Afroz <afrozalikhan81@gmail.com>
2023-12-18 12:01:32 +0530

    First shell script

--- 02-print.sh
+++ 02-print.sh
@@ -3,8 +3,8 @@
  echo  Hello world
 
  echo -e"\e[31Hello in Red Color\e[0m"
- echo -e"\e[32Hello in Red Color\e[0m"
- echo -e"\e[33Hello in Red Color\e[0m"
- echo -e"\e[34Hello in Red Color\e[0m"
- echo -e"\e[35Hello in Red Color\e[0m"
- echo -e"\e[36Hello in Red Color\e[0m"
+ echo -e"\e[32Hello in Green Color\e[0m"
+ echo -e"\e[33Hello in Yellow color\e[0m"
+ echo -e"\e[34Hello in Blue color\e[0m"
+ echo -e"\e[35Hello in Magenta color\e[0m"
+ echo -e"\e[36Hello in Cyan\e[0m"
