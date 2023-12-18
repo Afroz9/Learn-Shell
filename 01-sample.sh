d32749c39d38a96e2b98c29b2ac6eaaa0639f511
Afroz <afrozalikhan81@gmail.com>
2023-12-18 11:51:22 +0530

    First shell script

--- 01-sample.sh
+++ 01-sample.sh
@@ -2,3 +2,4 @@ ls -l
 pwd
 
 yum list install | grep vim
+# This is just comment
---
+++ 02-print.sh
@@ -0,0 +1,3 @@
+ # To print a messages there are couple of commands, but echo is widely used one
+
+ echo  Hello world
