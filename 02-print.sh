0749be01030fba16e3d8e7b862c8da12215e1ba3
Afroz <afrozalikhan81@gmail.com>
2023-12-18 11:59:03 +0530

    First shell script

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,10 @@
  # To print a messages there are couple of commands, but echo is widely used one
 
  echo  Hello world
+
+ echo -e"\e[31Hello in Red Color\e[0m"
+ echo -e"\e[32Hello in Red Color\e[0m"
+ echo -e"\e[33Hello in Red Color\e[0m"
+ echo -e"\e[34Hello in Red Color\e[0m"
+ echo -e"\e[35Hello in Red Color\e[0m"
+ echo -e"\e[36Hello in Red Color\e[0m"
