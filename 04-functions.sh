6f8a63ff94155246ace41aaaf16c363fb200d787
Afroz <afrozalikhan81@gmail.com>
2023-12-27 10:50:00 +0530

    RabbitMQ

--- 04-functions.sh
+++ 04-functions.sh
@@ -3,3 +3,11 @@ print_hello() {
 }
 
 print_hello
+
+print_argument() {
+  echo First Argument $1
+  echo Second Argument $2
+  echo All Argument $*
+  echo No of Argument $#
+}
+Print_Argument abc 123 xyz
