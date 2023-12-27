8b1088f5428e3c9f63f2faa59180528416e9e850
Afroz <afrozalikhan81@gmail.com>
2023-12-27 10:35:43 +0530

    RabbitMQ

--- 03-vars.sh
+++ 03-vars.sh
@@ -3,3 +3,12 @@ a=10
 echo value of a is $adollars
 echo value of a is ${a}dollars
 # Alter
+
+
+# Special variables for inputs
+# Special variables used for input are $0 to $n, $* $#
+
+echo $0
+echo $n
+echo $*
+echo $#
